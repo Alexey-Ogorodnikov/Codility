c0d636fa62f6cb1ba40450370684b9dec6d1f404
Alexey O <47650547+Alexey-Ogorodnikov@users.noreply.github.com>
2022-10-02 05:35:34 -0400

    CyclicRotation
    CyclicRotation
    Rotate an array to the right by a given number of steps.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
+
+
 fun main() {
-    println("main")
+    println("start")
 
     val binaryGap = BinaryGap()
     binaryGap.solution( 32 )
 
-
-
+    val arrayRotation = ArrayRotation()
+    arrayRotation.solution( intArrayOf(3, 8, 9, 7, 6),-3 )
 
 }
