7cd0c9cb13d38b54cd455f58e83ee02dc418127f
Alexey O <47650547+Alexey-Ogorodnikov@users.noreply.github.com>
2022-10-15 06:04:30 -0400

    missing element in array
    Find the missing element in a given permutation.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,21 +3,23 @@ import kotlin.random.Random
 fun main() {
     println("start")
 
-    val binaryGap = BinaryGap()
-    binaryGap.solution( 32 )
+//    val binaryGap = BinaryGap()
+//    binaryGap.solution( 32 )
+//
+//    val arrayRotation = ArrayRotation()
+//    arrayRotation.solution( intArrayOf(3, 8, 9, 7, 6),-3 )
+//
+//    val oddOccurrencesInArray = OddOccurrencesInArray()
+//    oddOccurrencesInArray.solution( intArrayOf(9, 3, 9, 3, 9, 7, 9) )
+//
+//
+//    val frogJump = FrogJump()
+//    frogJump.solution( 10, 85, 30 )
+//    frogJump.solution( 1, 5, 2 )
+//    frogJump.solution(5, 1000000000, 2)
+//    frogJump.solution( Random.nextInt(1,1000000000), Random.nextInt(1,1000000000), Random.nextInt(1,1000000000) )
 
-    val arrayRotation = ArrayRotation()
-    arrayRotation.solution( intArrayOf(3, 8, 9, 7, 6),-3 )
-
-    val oddOccurrencesInArray = OddOccurrencesInArray()
-    oddOccurrencesInArray.solution( intArrayOf(9, 3, 9, 3, 9, 7, 9) )
-
-
-    val frogJump = FrogJump()
-    frogJump.solution( 10, 85, 30 )
-    frogJump.solution( 1, 5, 2 )
-    frogJump.solution(5, 1000000000, 2)
-    frogJump.solution( Random.nextInt(1,1000000000), Random.nextInt(1,1000000000), Random.nextInt(1,1000000000) )
-
+    val missingElement = MissingElement()
+    missingElement.solution(intArrayOf(2,3,4,5,6,8))
 
 }
